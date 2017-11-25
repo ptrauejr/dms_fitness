c0aab344f3524d557ddd3ad8fc4b11020f5f33d2
Paul Traue <ptrauejr@gmail.com>
2017-11-25 12:41:58 -0600

    Adds basic auth via oath to the app

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -9,11 +9,19 @@ class Dashboard extends Component {
 
     render() {
         return (
-            <div className="container">
+            <div>
+                <div className="container">
                 <header className="text-center">
                     <h1 className="page-header">React Fit</h1>
                     <p className="lead">Your personal fitness dashboard</p>
                 </header>
+                </div>
+
+                <div className="row text-center">
+                    <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CGCB&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
+                        Log in with fitbit
+                    </a>
+                </div>
             </div>
         )
     }
