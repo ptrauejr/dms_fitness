bf2c9e46d4fa49378c50c99f21c55d0b18f9d277
Paul Traue <ptrauejr@gmail.com>
2017-11-26 17:42:56 -0600

    Changes some keys in the Steps component appropriately

--- app/javascript/packs/Steps.jsx
+++ app/javascript/packs/Steps.jsx
@@ -5,14 +5,13 @@ const Steps = ({steps}) =>
     <div className="panel panel-default">
         <div className="panel-heading">Steps</div>
         <div className="panel-body">
-            <BarChart width={600} height={600} data={data}
+            <BarChart width={600} height={600} data={steps["activites-steps"]}
                       margin={{top: 5, right: 30, left:20, bottom: 5}}>
-                <XAxis dataKey="name"/>
+                <XAxis dataKey="dateTime"/>
                 <YAxis/>
                 <CartisianGrid strokeDasharray="3 3"/>
                 <Tooltip/>
-                <Bar dataKey="pv" fill="#8884d8" />
-                <Bar dataKey="uv" fill="#82ca9d" />
+                <Bar dataKey="value" fill="#8884d8" />
             </BarChart>
         </div>
     </div>
