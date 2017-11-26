ebb00486eddf2c957b5a8a5be70ce9701d6d855b
Paul Traue <ptrauejr@gmail.com>
2017-11-26 16:28:38 -0600

    Adds new badges info to default data

--- app/javascript/packs/dummyData.js
+++ app/javascript/packs/dummyData.js
@@ -2,7 +2,8 @@ const dummyData = {
     user: {user: ''},
     loggedIn: false,
     lifetimeStats: {"best": {"total": {"distance": {"date":"", "value":""}, "steps": {"data":"","value":""}}},
-                   "lifetime": {"total": {"distance":"", "steps":""}}}
+                   "lifetime": {"total": {"distance":"", "steps":""}}},
+    badges: {badges: []},
 }
 
 export default dummyData
