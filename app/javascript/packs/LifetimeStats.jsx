963350695c22a3487d2943368dd18fe3888d98f1
Paul Traue <ptrauejr@gmail.com>
2017-11-26 16:03:02 -0600

    Adds the new hash to the LifetimeStats component

--- app/javascript/packs/LifetimeStats.jsx
+++ app/javascript/packs/LifetimeStats.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const LifetimeStats = ({lifetimeTotals, lifetimeBest}) =>
+const LifetimeStats = ({lifetimeStats}) =>
     <div className="panel panel-default">
         <div className="panel-heading"><h4>Lifetime Stats</h4></div>
             <div className="panel-body">
                 <h4>Distance</h4>
-                <p>Total: {lifetimeTotals.distance} miles</p>
-                <p>Best: {lifetimeBest.distance.value} on {lifetimeBest.steps.date}</p>
+                <p>Total: {lifetimeStats.lifetime.total.distance} miles</p>
+                <p>Best: {lifetimeStats.best.total.distance.value} on {lifetimeStats.best.total.distance.date}</p>
                 <h4>Steps</h4>
-                <p>Total: {lifetimeTotals.steps}</p>
-                <p>Best: {lifetimeBest.steps.value} on {lifetimeBest.steps.date}</p>
+                <p>Total: {lifetimeStats.lifetime.total.steps}</p>
+                <p>Best: {lifetimeStats.best.total.steps.value} on {lifetimeStats.best.total.steps.date}</p>
             </div>
     </div>
 
