fa9118f17cda92616e22ae5b7d20bc2dc45d85f8
Paul Traue <ptrauejr@gmail.com>
2017-11-26 18:06:12 -0600

    Adds range specifier so data gets graphed

--- app/javascript/packs/Steps.jsx
+++ app/javascript/packs/Steps.jsx
@@ -5,10 +5,10 @@ const Steps = ({steps}) =>
     <div className="panel panel-default">
         <div className="panel-heading">Steps</div>
         <div className="panel-body">
-            <BarChart width={600} height={600} data={steps["activities-steps"]}
+            <BarChart width={500} height={300} data={steps["activities-steps"]}
                       margin={{top: 5, right: 30, left:20, bottom: 5}}>
                 <XAxis dataKey="dateTime"/>
-                <YAxis/>
+                <YAxis domain={[0, 8000]}/>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Tooltip/>
                 <Bar dataKey="value" fill="#8884d8" />
