bf426bc1d0f261449a9657c02cde6bce0c693dcb
Paul Traue <ptrauejr@gmail.com>
2017-11-26 19:24:18 -0600

    Adds the image and name to the Friends component

--- app/javascript/packs/Friends.jsx
+++ app/javascript/packs/Friends.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 const Friends = ({friends}) =>
     <div className="panel panel-default">
         <div className="panel-heading"><h4>Friends</h4></div>
-        <div className="panel-body">
+        <ul className="panel-body list-group">
             {friends.map((friend, i) => {
                 return(
-                    <div key={i}>
-                    </div>
+                    <li key={i} className="list-group-item">
+                        <span className="badge">{friend.average.steps}</span>
+                        <img src={friend.user.avatar} />
+                        <h5>{friend.user.displayName}</h5>
+                    </li>
                 )
             })}
-        </div>
+        </ul>
     </div>
 
 export default Friends
